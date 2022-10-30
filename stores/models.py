36ef9c33e61404798dde75303d548f4ebbb0d1fe
JiaXin Yang <64412891+qixia1998@users.noreply.github.com>
2022-10-30 21:02:15 +0800

    Update models.py

--- stores/models.py
+++ stores/models.py
@@ -8,21 +8,24 @@ from django.core.validators import RegexValidator
 
 
 class Pizzeria(models.Model):
-    pizzeria_name = models.CharField(max_length=200, blank=False)
+    pizzeria_name = models.CharField(max_length=200,
+        blank=False)
     street = models.CharField(max_length=400, blank=True)
     city = models.CharField(max_length=400, blank=True)
-    state = models.CharField(max_length=2, null=True, blank=True)
+    state = models.CharField(max_length=2, null=True,
+        blank=True)
     zip_code = models.IntegerField(blank=True, default=0)
     website = models.URLField(max_length=420, blank=True)
-    phone_number = models.CharField(validators=[RegexValidator(regex=r'^\1?\d{9,10}$')], 
-            max_length=10,
-            blank=True
-            )
+    phone_number = models.CharField(
+    validators=[RegexValidator(regex=r'^\1?\d{9,10}$')],
+        max_length=10,
+        blank=True
+        )
     description = models.TextField(blank=True)
     logo_image = models.ImageField(
-            uplod_to='pizzariaImages',
-            blank=True,
-            default='pizzariaImages/pizzalogo.png'
-            )
+    upload_to='pizzariaImages',
+    blank=True,
+    default="pizzariaImages/pizzalogo.png"
+    )
     email = models.EmailField(max_length=245, blank=True)
     active = models.BooleanField(default=True)
